bbfe0f4c87f76975bf850adbd8adee04424885bb
Josh Long <josh@joshlong.com>
2021-12-18 16:39:38 -0800

    up

--- src/main/java/com/example/graphqlnative/GraphqlNativeApplication.java
+++ src/main/java/com/example/graphqlnative/GraphqlNativeApplication.java
@@ -1,16 +1,5 @@
 package com.example.graphqlnative;
 
-import graphql.GraphQL;
-import graphql.analysis.QueryVisitorFieldArgumentEnvironment;
-import graphql.analysis.QueryVisitorFieldArgumentInputValue;
-import graphql.execution.Execution;
-import graphql.execution.nextgen.result.RootExecutionResultNode;
-import graphql.language.*;
-import graphql.parser.ParserOptions;
-import graphql.schema.*;
-import graphql.schema.validation.SchemaValidationErrorCollector;
-import graphql.util.NodeAdapter;
-import graphql.util.NodeZipper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.graphql.boot.GraphQlSourceBuilderCustomizer;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
@@ -28,100 +18,8 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import static org.springframework.nativex.hint.TypeAccess.*;
-
-@TypeHint(
-	typeNames = {
-		"graphql.analysis.QueryTraversalContext",
-		"graphql.schema.idl.SchemaParseOrder",
-	},
-	types = {
-
-		Argument.class,
-		ArrayValue.class,
-		Boolean.class,
-		BooleanValue.class,
-		Customer.class,
-		DataFetchingEnvironment.class,
-		Directive.class,
-		DirectiveDefinition.class,
-		DirectiveLocation.class,
-		Document.class,
-		EnumTypeDefinition.class,
-		EnumTypeExtensionDefinition.class,
-		EnumValue.class,
-		EnumValueDefinition.class,
-		Execution.class,
-		Field.class,
-		FieldDefinition.class,
-		FloatValue.class,
-		FragmentDefinition.class,
-		FragmentSpread.class,
-		GraphQL.class,
-		GraphQLArgument.class,
-		GraphQLCodeRegistry.Builder.class,
-		GraphQLDirective.class,
-		GraphQLEnumType.class,
-		GraphQLEnumValueDefinition.class,
-		GraphQLFieldDefinition.class,
-		GraphQLInputObjectField.class,
-		GraphQLInputObjectType.class,
-		GraphQLInterfaceType.class,
-		GraphQLList.class,
-		GraphQLNamedType.class,
-		GraphQLNonNull.class,
-		GraphQLObjectType.class,
-		GraphQLOutputType.class,
-		GraphQLScalarType.class,
-		GraphQLSchema.class,
-		GraphQLSchemaElement.class,
-		GraphQLUnionType.class,
-		ImplementingTypeDefinition.class,
-		InlineFragment.class,
-		InputObjectTypeDefinition.class,
-		InputObjectTypeExtensionDefinition.class,
-		InputValueDefinition.class,
-		IntValue.class,
-		InterfaceTypeDefinition.class,
-		InterfaceTypeExtensionDefinition.class,
-		List.class,
-		ListType.class,
-		NodeAdapter.class,
-		NodeZipper.class,
-		NonNullType.class,
-		NullValue.class,
-		ObjectField.class,
-		ObjectTypeDefinition.class,
-		ObjectTypeExtensionDefinition.class,
-		ObjectValue.class,
-		OperationDefinition.class,
-		OperationTypeDefinition.class,
-		ParserOptions.class,
-		QueryVisitorFieldArgumentEnvironment.class,
-		QueryVisitorFieldArgumentInputValue.class,
-		RootExecutionResultNode.class,
-		ScalarTypeDefinition.class,
-		ScalarTypeExtensionDefinition.class,
-		SchemaDefinition.class,
-		SchemaExtensionDefinition.class,
-		SchemaValidationErrorCollector.class,
-		SelectionSet.class,
-		StringValue.class,
-		TypeDefinition.class,
-		TypeName.class,
-		UnionTypeDefinition.class,
-		UnionTypeExtensionDefinition.class,
-		VariableDefinition.class,
-		VariableReference.class,
-
-		// new
-	}, //
-	access = {//
-		PUBLIC_CLASSES, PUBLIC_CONSTRUCTORS, PUBLIC_FIELDS, PUBLIC_METHODS,
-		DECLARED_CLASSES, DECLARED_CONSTRUCTORS, DECLARED_FIELDS, DECLARED_METHODS
-	} //
-)
-@ResourceHint(patterns = {"graphql/schema.graphqls", "graphiql/index.html"})
+@ResourceHint(patterns = {"graphql/schema.graphqls" })
+@TypeHint(types = Customer.class, access = {TypeAccess.DECLARED_METHODS, TypeAccess.DECLARED_CONSTRUCTORS})
 @SpringBootApplication
 public class GraphqlNativeApplication {
 
